e51c9cb0ace0d01890f2401451fe0ea67ed46185
I D Neel <idkneel@gmail.com>
2023-09-22 17:55:26 -0400

    remove URL or image done

--- app/src/main/java/com/example/notes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/notes/activities/CreateNoteActivity.java
@@ -103,6 +103,16 @@ public class CreateNoteActivity extends AppCompatActivity
             setViewOrUpdateNote();
         }
 
+        findViewById(R.id.imageRemoveImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageNote.setImageBitmap(null);
+                imageNote.setVisibility(View.GONE);
+                findViewById(R.id.imageRemoveImage).setVisibility(View.GONE);
+                selectedImagePath = "";
+            }
+        });
+
         initMiscellaneous();
         setSubtitleIndicatorColor();
     }
@@ -116,6 +126,7 @@ public class CreateNoteActivity extends AppCompatActivity
         if(alreadyAvailableNote.getImagePath() != null && !alreadyAvailableNote.getImagePath().trim().isEmpty()) {
             imageNote.setImageBitmap(BitmapFactory.decodeFile(alreadyAvailableNote.getImagePath()));
             imageNote.setVisibility(View.VISIBLE);
+            findViewById(R.id.imageRemoveImage).setVisibility(View.VISIBLE);
             selectedImagePath = alreadyAvailableNote.getImagePath();
         }
 
@@ -335,6 +346,7 @@ public class CreateNoteActivity extends AppCompatActivity
                         Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
                         imageNote.setImageBitmap(bitmap);
                         imageNote.setVisibility(View.VISIBLE);
+                        findViewById(R.id.imageRemoveImage).setVisibility(View.VISIBLE);
 
                         selectedImagePath = getPathFromUri(selectedImageUri);
                     } catch (Exception exception) {
